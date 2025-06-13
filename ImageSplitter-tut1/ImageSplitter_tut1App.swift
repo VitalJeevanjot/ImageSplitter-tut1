43fc6823e97646281c5355775790dae518c054e1
VitalJeevanjot <jeevanjotsinghvital@gmail.com>
2025-06-13 23:35:24 +0530

    Initial Commit

---
+++ ImageSplitter-tut1/ImageSplitter_tut1App.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSplitter_tut1App.swift
+//  ImageSplitter-tut1
+//
+//  Created by Jeevanjot Singh on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageSplitter_tut1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
