ac0e859357499b02acbb60a531b0816792880dba
VitalJeevanjot <jeevanjotsinghvital@gmail.com>
2025-07-01 15:13:37 +0530

    Image splitter commit

---
+++ ImageSplitter-tut1/MagnificatioTesting.swift
@@ -0,0 +1,18 @@
+//
+//  MagnificatioTesting.swift
+//  ImageSplitter-tut1
+//
+//  Created by Jeevanjot Singh on 30/06/25.
+//
+
+import SwiftUI
+
+struct MagnificatioTesting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MagnificatioTesting()
+}
---
+++ ImageViews.swift
@@ -0,0 +1,18 @@
+//
+//  ImageViews.swift
+//  ImageSplitter-tut1
+//
+//  Created by Jeevanjot Singh on 30/06/25.
+//
+
+import SwiftUI
+
+struct ImageViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageViews()
+}
